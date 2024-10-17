e8bc1c13295905d9dc41b8b55fac4f8606d3a73e
sagar-shirwalkar <shirwalkarsagar0@gmail.com>
2024-10-17 14:51:35 +0530

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/sagar-shirwalkar.github.io/',
+  plugins: [react()],
+  build: {
+    minify: false,
+    sourcemap: true, // Enable source maps in production
+  },
+})
