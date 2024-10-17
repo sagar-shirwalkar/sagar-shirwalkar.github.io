22b11f32a0e73ea2ac419a93aacf43c3b3fb66df
sagar-shirwalkar <shirwalkarsagar0@gmail.com>
2024-10-17 15:58:01 +0530

    rebuild pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/sagar-shirwalkar.github.io/' : '/',
+  base: process.env.NODE_ENV === 'production' ? 'https://sagar-shirwalkar.github.io/' : '/',
   plugins: [react()],
   build: {
     minify: false,
